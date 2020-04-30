3c6a073ab683c5255f992fed6c1e9cc648058749
skjerns <14980558+skjerns@users.noreply.github.com>
2020-04-30 17:50:27 +0200

    increase v

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
      name='mat73',  
-     version='0.38',
+     version='0.39',
      author="skjerns",
      author_email="nomail@nomail.com",
      description="Load MATLAB .mat 7.3 into Python native data types",
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/skjerns/mat7.3",
-     download_url="https://github.com/skjerns/mat7.3/archive/v.038.tar.gz",
+     download_url="https://github.com/skjerns/mat7.3/archive/v.039.tar.gz",
      install_requires=['h5py', 'numpy'],
      license='GNU 2.0',
      packages=['mat73'],
